8ff579c42f8813bd3b2789e0cf76128b1ba7267e
austen0 <austen.r.casey@gmail.com>
2017-03-27 13:53:05 -0700

    .

--- README.md
+++ README.md
@@ -8,8 +8,12 @@ the following mods:
 * [GhostDog's PST:UI mod (v2.2)][gdui]
 * [Qwinn's PS:T Unfinished Business (v4.12)][qub]
 
+Major props to this [blog post][neant] on /dev/neant for providing the
+instructions carried out by this script.
+
 
 [bws]: http://www.gibberlings3.net/widescreen/
 [gdui]: http://www.shsforums.net/files/file/683-ghostdogs-pst-ui-mod-v22/
+[neant]: http://www.neant.ro/2011/12/planescapetorment-on-linux/
 [qub]: http://www.shsforums.net/files/file/648-pst-unfinished-business-v412/
 [wd]: http://weidu.org/
--- install.sh
+++ install.sh
@@ -7,12 +7,13 @@
 # To specify a different path for the game folder:
 #   $ ./install.sh /path/to/game/directory
 
+set -e
 
 # Get full path to project directory
 readonly SRC_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
 # Set which WeiDU executables to use
-if [ "$(uname -m)" == "x86_64" ]; then
+if [[ "$(uname -m)" == "x86_64" ]]; then
   weidu_bin=$SRC_DIR/weidu-linux/bin/amd64
 else
   weidu_bin=$SRC_DIR/weidu-linux/bin/i386
@@ -23,24 +24,51 @@ PATH=$PATH:$weidu_bin
 if [ ! -z ${1} ]; then
   pst_dir=${1}
 else
-  pst_dir=${HOME}/GOG Games/Planescape Torment/prefix/drive_c/GOG Games/Planescape Torment
+  pst_dir=${HOME}/GOG\ Games/Planescape\ Torment/prefix/drive_c/GOG\ Games/Planescape\ Torment
 fi
 
+# Check dependencies.
+echo "Verifying playonlinux is installed..."
+if command -v playonlinux > /dev/null; then
+  echo "Done!"
+else
+  echo "ERROR: playonlinux not installed" >&2
+  exit 1
+fi
+
+# Verify directory exists.
+echo "Verifying game directory exists..."
+if [[ ! -d "$pst_dir" ]]; then
+  echo "ERROR: The provided game directory does not exist: ${pst_dir}" >&2
+  exit 1
+fi
+echo "Done!"
+
 # Change all filenames in game directory to lowercase.
-cd $pst_dir
+cd "$pst_dir"
 yes | tolower
 
 # Create linux.ini file.
-cp -f ${SRC_DIR}/linux.ini.tmpl ${pst_dir}/linux.ini
-sed -ri "s|%pst_dir%|${pst_dir}|g" ${pst_dir}/linux.ini
+cp -f "${SRC_DIR}/linux.ini.tmpl" "${pst_dir}/linux.ini"
+sed -ri "s|%pst_dir%|${pst_dir}|g" "${pst_dir}/linux.ini"
 
 # Copy mod data to game directory.
-cp -r ${SRC_DIR}/mod_data/* ${pst_dir}
+cp -r "${SRC_DIR}/mod_data"/* "${pst_dir}"
 
-# Install widescreen mod.
-cp ${pst_dir}/data/* ${pst_dir}/cache/
+# Give user a heads up mods are about to install.
+echo -e "\e[1;92mATTENTION:\e[25m User in put required during mod installation!\e[0m"
+read -n1 -r -p "Press any key to continue..." key
+
+# Install widescreen mod. Files from data/ are moved into cache/ temporarily
+# during mod installation. No idea why this is necessary but it is.
+echo "Installing widescreen mod..."
+if [[ ! -d "${pst_dir}/cache" ]]; then
+  mkdir "${pst_dir}/cache"
+fi
+cp "${pst_dir}/data"/* "${pst_dir}/cache/"
 weinstall widescreen
-rm ${pst_dir}/cache/*.bif
+rm "${pst_dir}/cache/"*.bif
+echo "Done!"
 
 # Install the rest of the mods.
 weinstall setup-ghostdogs-pst-ui.tp2
